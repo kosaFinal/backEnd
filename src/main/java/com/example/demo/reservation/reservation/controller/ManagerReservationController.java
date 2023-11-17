b93ae58293275fc35285ad626133e8dbfb8af2ea
ckdud9299 <58415917+ckdud9299@users.noreply.github.com>
2023-11-17 09:52:47 +0900

    chore: 점주 예약 url manager로 변경

--- src/main/java/com/example/demo/reservation/reservation/controller/ManagerReservationController.java
+++ src/main/java/com/example/demo/reservation/reservation/controller/ManagerReservationController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/admin/reservation")
+@RequestMapping("/manager/reservation")
 @Slf4j
 public class ManagerReservationController {
 
